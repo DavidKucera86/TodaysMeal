c55d07e9bd658f50e5457651f5df7536747d7992
DavidKucera <dvdkcr@seznam.cz>
2023-02-23 15:20:16 +0100

    Searching at movie section finished

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import { FaInfoCircle } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import allMovies from '../data'
+import React, { useEffect, useState } from "react";
+import { FaInfoCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import allMovies from "../data";
 
 const Movies = () => {
+    const [searchingMovie, setSearchingMovie] = useState("")
+    const [filteredMovies, setFilteredMovies] = useState([])
+
+    useEffect(() => {
+        const finalMovies = allMovies.filter((movie) => {
+            return movie.title.toLowerCase().includes(searchingMovie.toLocaleLowerCase())
+        })
+
+        setFilteredMovies(finalMovies)
+    },[searchingMovie])
+
   return (
     <main>
-        <h2>Which movie to the meal do you wish?</h2>
-        <section>
-            {
-                allMovies.map((oneMovie) => {
-                    const {id, image, title} = oneMovie
-                    return <article key={id}>
-                        <h3>{title}</h3>
-                        <img src={image} alt={title} />
-                        <Link to={`/detail/${id}`}>
-                        <FaInfoCircle />
-                        <span>More info</span>
-                        </Link>
-                    </article>
-                })
-            }
-        </section>
+      <h2>Which movie to the meal do you wish?</h2>
+      <form>
+        <input type="text" placeholder="Search for a Movie"
+            value={searchingMovie}
+            onChange={(e) => setSearchingMovie(e.target.value)}
+        />
+      </form>
+      <section>
+        {filteredMovies.map((oneMovie) => {
+          const { id, image, title } = oneMovie;
+          return (
+            <article key={id}>
+              <h3>{title}</h3>
+              <img src={image} alt={title} />
+              <Link to={`/detail/${id}`}>
+                <FaInfoCircle />
+                <span>More info</span>
+              </Link>
+            </article>
+          );
+        })}
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
